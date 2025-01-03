aa0ea00a7a42968317fe9ac96521d3a94edb1a62
Sonic Ma <sonic.ma@outlook.com>
2025-01-03 17:07:10 +0800

    fdf

--- k8s.py
+++ k8s.py
@@ -1,11 +1,59 @@
 import yaml
+import wget
 import os
 import docker
 from docker.errors import APIError, NotFound
+from github import Github
 
 # 读取 all.yml 文件 
 with open('group_vars/all.yml', 'r', encoding='utf-8') as file:
     data = yaml.load(file, Loader=yaml.FullLoader)
+
+k8s_version = data['kubernetes']['version']
+base_url = f'https://dl.k8s.io/release/{k8s_version}/bin/linux/amd64'
+
+binaries = [
+    'kube-apiserver',
+    'kube-controller-manager',
+    'kubectl',
+    'kubelet',
+    'kube-proxy',
+    'kube-scheduler'
+]
+
+save_dir = './k8s_binaries'
+os.makedirs(save_dir, exist_ok=True)
+
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+REPO_NAME = os.environ.get('GITHUB_REPOSITORY')
+
+# 初始化 GitHub 客户端
+g = Github(GITHUB_TOKEN)
+repo = g.get_repo(REPO_NAME)
+
+# 检查 Release 是否存在
+try:
+    release = repo.get_release(k8s_version)
+    print(f'Release {k8s_version} already exists. Skipping upload.')
+except:
+    release = repo.create_git_release(k8s_version, f'Release {k8s_version}', "Release description")
+    print(f'Created new release {k8s_version}.')
+
+# 遍历二进制文件列表并下载
+for binary in binaries:
+    url = f'{base_url}/{binary}'
+    save_path = os.path.join(save_dir, binary)
+    try:
+        print(f'Downloading {binary} from {url}')
+        wget.download(url, save_path)
+        print(f'Successfully downloaded {binary} to {save_path}')
+
+        # 上传到 GitHub Release
+        with open(save_path, 'rb') as file:
+            release.upload_asset(file, name=binary)
+        print(f'Successfully uploaded {binary} to GitHub Release')
+    except Exception as e:
+        print(f'An error occurred while downloading or uploading {binary}: {e}')
 
 # 阿里云 ACR 镜像仓库信息
 acr_repo = 'registry.cn-hangzhou.aliyuncs.com'
